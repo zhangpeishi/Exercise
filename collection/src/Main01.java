72f82ac35b928da1b3d35baa20b108e0376257d1
zhangpeishi <74777311+zhangpeishi@users.noreply.github.com>
2021-01-24 18:35:52 +0800

    练习

--- collection/src/Main01.java
+++ collection/src/Main01.java
@@ -19,8 +19,41 @@ import java.util.ArrayList;
  *
  * unworked:使用Set去重特性优化
  *
+ *
  */
+/*原题目：
+public class Main {
+    public static void main(String[] args) {
+        List<Message> received = List.of(
+            new Message(1, "Hello!"),
+            new Message(2, "发工资了吗？"),
+            new Message(2, "发工资了吗？"),
+            new Message(3, "去哪吃饭？"),
+            new Message(3, "去哪吃饭？"),
+            new Message(4, "Bye")
+        );
+        List<Message> displayMessages = process(received);
+        for (Message message : displayMessages) {
+            System.out.println(message.text);
+        }
+    }
 
+    static List<Message> process(List<Message> received) {
+        // TODO: 按sequence去除重复消息
+        return received;
+    }
+}
+
+class Message {
+    public final int sequence;
+    public final String text;
+    public Message(int sequence, String text) {
+        this.sequence = sequence;
+        this.text = text;
+    }
+}
+ */
+//解答：
 public class Main01 {
 
     public static void main (String[]args){
